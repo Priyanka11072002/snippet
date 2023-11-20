5ce32898b624c7606f3caeeb976b5e168e820175
Priyanka11072002 <pk796395+pk@gmail.com>
2023-11-20 10:19:39 +0530

    changes

---
+++ ImportantNotepadLinks.js
@@ -0,0 +1,55 @@
+//https://themeforest.net     //https://www.thecareerwallet.com/
+//https://codesandbox.io/s/summer-firefly-mrjtn9?file=/src/App.js                                 //searchbar searchbar with the filter
+//https://codesandbox.io/s/zealous-yonath-s9284s?file=/src/App.js                                   //searchbar searchbar with the filter      
+
+https://codesandbox.io/s/react-typescript-forked-2jxn36?file=/src/App.tsx:0                    //search feild with props and custoom hooks 
+////https://codesandbox.io/s/react-typescript-forked-mnpxmh?file=/src/Button.ts           //typescript (extends,omit,inheritance ,buttonhtmlattributes)
+https://codesandbox.io/s/confident-bardeen-2hxgsd?file=/src/debouncing/throtting.js:336-345
+https://codesandbox.io/s/confident-bardeen-2hxgsd?file=/src/lazyLoading/parentMem     //usefeild(meta,feild),jsonstringify,callback,lazyloading
+
+https://codesandbox.io/s/sharp-tdd-hwxqwm?file=/src/App.js                                     //sidebar using react sidebar plugin
+
+https://codesandbox.io/embed/reverent-cherry-5ssph4?file=/src/App.js&codemirror=1                              //useref
+<script src="https://kit.fontawesome.com/3444ab5dee.js" crossorigin="anonymous"></script>                      //fontawesome link                  
+
+https://codesandbox.io/s/determined-goldwasser-573cjz?file=/src/App.js                                         //dummypage of product api
+
+https://codesandbox.io/s/hidden-cloud-czp4x6?file=/src/App.js                 //useMemo hook
+https://www.hackerrank.com/contests/7days-javascript/challenges
+
+https://codesandbox.io/s/modest-noyce-qryq65?file=/src/App.js               //useRef hook
+https://codesandbox.io/s/confident-bardeen-2hxgsd?file=/src/ReactHooks/UseRef.js // reacthooks  https://codesandbox.io/s/confident-bardeen-2hxgsd?file=/src/App.js:449-479
+https://codesandbox.io/s/beautiful-star-lhyrf8?file=/src/ReactHooks/useRef.js:265-316           //useRef,memo,useCallback,useEffect hook
+https://codesandbox.io/s/hidden-feather-rhfmsn?file=/src/App.js         //usereducer inc,dec,reset
+
+//https://codesandbox.io/s/cool-greider-x25dy0?file=/src/Card.js                                 //card component   
+//https://codesandbox.io/s/rough-haze-jtdf8r?file=/src/App.js                     //useContext
+https://codesandbox.io/s/affectionate-fast-z45ky6?file=/src/App.js
+https://codesandbox.io/p/github/Priyanka11072002/projectUsingReducer/main?create=true&file=/src/App.js:5,39    //GITHUB
+//https://codesandbox.io/s/admiring-microservice-3r9qpy?file=/src/App.js             //todolist
+https://codesandbox.io/s/hidden-feather-rhfmsn?file=/src/reducers/counterReducer.js           //useReducer
+//https://codesandbox.io/s/rough-haze-jtdf8r?file=/src/about.js          //notestate,store context
+
+//https://codesandbox.io/s/frosty-brook-xpp86s?file=/src/App.js           //calcucate square root  
+
+https://codesandbox.io/s/xenodochial-engelbart-j9dlp2?file=/src/App.js   //project using useReducer
+https://codesandbox.io/s/xenodochial-engelbart-j9dlp2?file=/src/App.js
+https://codesandbox.io/s/serene-framework-2pjwfp?file=/src/index.js             //html,css,js addeventlistener
+https://codesandbox.io/s/peaceful-stitch-yhdvs6?file=/src/App.js           //calculator
+
+https://codesandbox.io/s/react-redux-767wzl?file=/src/App.js             //redux toolkit 
+https://codesandbox.io/s/damp-dream-twtsnr?file=/src/store/reduxStore.tsx      //usereducer with typescript and useContext
+           
+https://codesandbox.io/s/frosty-waterfall-8fdjdv?file=/src/App.js            // edit,delete, ...spread operator ,&& ,conditionally perform;
+
+to start local server
+json-server --watch db.json --port 8080
+npx json-server --watch db.json --port 9020  
+
+select_from_select_database@g.com
+!@#$%^&*()_
+Node.js Express role-based authentication tutorial
+//$env:NODE_OPTIONS = "--openssl-legacy-provider"
+//npc create-react-app myapp --template redux-typescript code.
+
+https://www.thatjsdude.com/interview/js1.html
